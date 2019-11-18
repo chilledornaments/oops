e570ee8cf010db6c486699422d48a9d15d9bf473
mitchya1 <mitchell.anderson@colorado.edu>
2019-11-18 14:32:48 -0700

    actually fix slack issue

--- server.go
+++ server.go
@@ -38,10 +38,6 @@ func createSecret(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
 
-		if strings.Contains(r.UserAgent(), "Slackbot") {
-			w.Write([]byte("Hello Slack"))
-		}
-
 		templateBox, err := rice.FindBox("templates")
 
 		templateString, err := templateBox.String("create.html.tmpl")
@@ -108,13 +104,21 @@ func createSecret(w http.ResponseWriter, r *http.Request) {
 func showSecret(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
-		id := strings.TrimPrefix(r.URL.Path, "/secret/")
-		secret, err := db.ReturnSecret(id)
 
-		if err != nil {
-			w.Write([]byte(secret + "\n"))
+		if strings.Contains(r.UserAgent(), "Slack") {
+			log.Println("Ignored Slack link expansion")
+			w.Write([]byte("Hello Slack"))
 		} else {
-			w.Write([]byte(secret + "\n"))
+
+			id := strings.TrimPrefix(r.URL.Path, "/secret/")
+			secret, err := db.ReturnSecret(id)
+
+			if err != nil {
+				w.Write([]byte(secret + "\n"))
+			} else {
+				w.Write([]byte(secret + "\n"))
+			}
+
 		}
 
 	} else {
