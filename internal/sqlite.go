3f7515e1d28dc1bf1a6d88fbd4e932a8ff9fd0bb
mitchya1 <mitchell.anderson@colorado.edu>
2020-06-13 18:38:25 -0600

    add skeleton

---
+++ internal/sqlite.go
@@ -0,0 +1,14 @@
+package internal
+
+func AddSqliteSecret(secret string, expiration int64) (string, error) {
+
+	return "", nil
+}
+
+func ReturnSqliteSecret(id string) (string, error) {
+	return "", nil
+}
+
+func deleteSecret(id string) error {
+	return nil
+}
