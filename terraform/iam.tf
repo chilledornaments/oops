ee8bd5079987a6e1f0217b401384adca0c8b9ac1
mitchya1 <mitchell.anderson@colorado.edu>
2020-12-24 12:54:01 -0700

    format terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "oops_ec2_iam_role" {
-    name = "prod-oops-role"
-    assume_role_policy = <<EOF
+  name               = "prod-oops-role"
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -15,36 +15,36 @@ resource "aws_iam_role" "oops_ec2_iam_role" {
   ]
 }
 EOF
-    tags = {
-        Name = "prod-oops-role"
-        Environment = "prod"
-        ManagingTeam = "SysEng"
-        CostCenter = "SysEng"
-    }
+  tags = {
+    Name         = "prod-oops-role"
+    Environment  = "prod"
+    ManagingTeam = "SysEng"
+    CostCenter   = "SysEng"
+  }
 }
 
 // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/api-permissions-reference.html
 resource "aws_iam_policy" "oops_iam_policy" {
-    name = "prod-oops-policy"
-    policy = <<EOF
+  name   = "prod-oops-policy"
+  policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:DeleteItem",
-                "dynamodb:GetItem",
-                "dynamodb:PutItem"
-            ],
-            "Resource": "${aws_dynamodb_table.oops_table.arn}"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+      ],
+      "Resource": "${aws_dynamodb_table.oops_table.arn}"
+    }
+  ]
 }
 EOF
 }
 
 resource "aws_iam_role_policy_attachment" "oops_attach" {
-    role = aws_iam_role.oops_ec2_iam_role.arn
-    policy = aws_iam_policy.oops_iam_policy.arn
+  role   = aws_iam_role.oops_ec2_iam_role.arn
+  policy = aws_iam_policy.oops_iam_policy.arn
 }
